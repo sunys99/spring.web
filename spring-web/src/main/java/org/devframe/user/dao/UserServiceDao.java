8fa8f8549cee68342ffd853028a1c310accef4af
David Sun <sunys99@gmail.com>
2012-10-19 00:58:02 +0900

    a

--- spring-web/src/main/java/org/devframe/user/dao/UserServiceDao.java
+++ spring-web/src/main/java/org/devframe/user/dao/UserServiceDao.java
@@ -1,20 +1,22 @@
 package org.devframe.user.dao;
 
-import javax.annotation.Resource;
-
 import org.devframe.domain.User;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository("UserServiceDao")
 public class UserServiceDao{
 	
-	@Resource
-	private SessionFactory sessionFactory;
-
-	public void setUserInfo(User user){
-		
-		sessionFactory.getCurrentSession().save(user);
+	private HibernateTemplate hibernateTemplate;
+	
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory){
+		hibernateTemplate = new HibernateTemplate(sessionFactory);
 	}
 
+	public void setUserInfo(User user){
+		hibernateTemplate.saveOrUpdate(user);
+	}
 }
